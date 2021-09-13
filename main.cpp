6812b7b381998645d99fc11a3fa64c8aa9b32b62
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2021-09-13 10:56:54 -0400

    Added task A and task B

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
-// add your code as appropriate 
+/*
+Author: Gavin Xiao
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: 
+
+Here, briefly, at least in one or a few sentences
+describe what the program does.
+*/
+
 #include <iostream>
+#include <string>
+#include <fstream> 
+#include <cstdlib> 
+#include <climits>
+#include "reservoir.h"
 
-int main()
-{
-  
+int main() {
+    std::cout << "Task A:\n";
+    std::cout << "The east storage on 01/05/2018 is: " << get_east_storage("01/05/2018") << " billion gallons." << std::endl;
+    std::cout << "The east storage on 02/12/2018 is: " << get_east_storage("02/12/2018") << " billion gallons." << std::endl;
+    std::cout << "The east storage on 09/24/2018 is: " << get_east_storage("09/24/2018") << " billion gallons." << std::endl;
 
+    std::cout << "\nTask B:\n";
+    std::cout << "The maximum storage in the East is: " << get_max_east() << " billion gallons." << std::endl;
+    std::cout << "The minimum storage in the East is: " << get_min_east() << " billion gallons." << std::endl;
   
-  return 0;
+return 0;
 }
